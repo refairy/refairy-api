4af13fec0727f4202f110731ebc53f0229966734
sqrtpi177 <solar0037@gmail.com>
2020-11-22 19:32:12 +0900

    update: add debug options

--- refairy_api/__main__.py
+++ refairy_api/__main__.py
@@ -1,5 +1,10 @@
+import sys
 from .app import main
 
 
 if __name__ == '__main__':
-    main()
+    if sys.argv[1] in ['-d', '--debug']:
+        debug = True
+    else:
+        debug = False
+    main(debug)
--- refairy_api/app.py
+++ refairy_api/app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from .views.index import index as index_view
+from .views.check import check as check_view
 from .utils.get_env import get_env
 
 
-def main():
+def main(debug: bool=False):
     app = Flask(__name__)
 
-    app.add_url_rule('/', 'index', view_func=index_view)
+    app.add_url_rule('/', view_func=index_view)
+    app.add_url_rule('/check', view_func=check_view, methods=['POST'])
 
     port = get_env('PORT')
-    app.run('0.0.0.0', port)
+    app.run('0.0.0.0', port, debug=debug)
 
 
 if __name__ == '__main__':
