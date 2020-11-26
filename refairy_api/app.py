e47c6f132aed07740f615518b83d2ded65b85de8
sqrtpi177 <solar0037@gmail.com>
2020-11-26 22:12:46 +0900

    fix app:main

--- refairy_api/app.py
+++ refairy_api/app.py
@@ -3,13 +3,14 @@ from sanic import Sanic
 from .routes.index import index as index_route
 from .routes.check import check as check_route
 from .utils.get_env import get_env
+from .textAnalyzer.text_analyzer import analyzer
 
 
 def main(debug: bool=False):
     app = Sanic(__name__)
 
     app.add_route(index_route, '/')
-    app.add_route(check_route, '/check/', methods=['POST'])
+    app.add_route(check_route, '/check', methods=['POST'])
 
     port = get_env('PORT')
     app.run('0.0.0.0', port=port, debug=debug)
